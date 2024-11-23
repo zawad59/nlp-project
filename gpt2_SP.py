b3b6f754a58db0666c3baaea30319fa9cc2bee13
zawad59 <tawsikzawad@gmail.com>
2024-11-23 18:15:45 -0500

    Update gpt2_SP.py

--- gpt2_SP.py
+++ gpt2_SP.py
@@ -31,7 +31,7 @@ embedder = SentenceTransformer('all-MiniLM-L6-v2').to(device)
 # Load datasets
 train_data = np.load('SP_train 1.npy', allow_pickle=True)
 dev_data = np.load('SP_dev 1.npy', allow_pickle=True)
-test_data = np.load('SP_test 1.npy', allow_pickle=True)
+test_data = np.load('SP_test_combined.npy', allow_pickle=True)
 
 # Initialize NLTK tools
 stemmer = PorterStemmer()
